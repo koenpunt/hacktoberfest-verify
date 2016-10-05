8e0ada16626f0948cd44d86d67469702222de768
Koen Punt <koen@koenpunt.nl>
2016-10-05 20:20:38 +0200

    pass REDIS_URL to Predis

--- src/AppBundle/Service/RedisCacheService.php
+++ src/AppBundle/Service/RedisCacheService.php
@@ -11,9 +11,9 @@ class RedisCacheService
 {
     protected $client;
 
-    public function __construct()
+    public function __construct($url = null)
     {
-        $this->client = new Client();
+        $this->client = new Client($url);
     }
 
     public function save($key, $content)
